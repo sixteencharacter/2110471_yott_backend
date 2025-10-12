9e66df03d5e5f5ad5f74fec7d332d8b73b78cdeb
Amorn Phanturat <amornphanturat@Amorns-MacBook-Air.local>
2025-10-12 16:21:23 +0700

    fix minor problems

--- models/person.py
+++ models/person.py
@@ -5,10 +5,10 @@ class Person(Base):
     __tablename__ = "yott_person"
 
     uid = Column(UUID, primary_key=True)
-    email = Column(Text(), unique=True)
-    given_name = Column(Text(), unique=True)
-    family_name = Column(Text(), unique=True)
-    preferred_username = Column(Text(), unique=True)
+    email = Column(Text())
+    given_name = Column(Text())
+    family_name = Column(Text())
+    preferred_username = Column(Text())
 
     def __repr__(self):
         return f"id: {self.uid}, name: {self.given_name}"
