1f4514a97340495a47d6130788492f4ba0e62bc4
Amorn Phanturat <amornphanturat@Amorns-MacBook-Air.local>
2025-10-24 17:19:40 +0700

    added the chat member get and chat history

--- routers/v1/chatRouter.py
+++ routers/v1/chatRouter.py
@@ -1,11 +1,12 @@
-from fastapi import APIRouter , Depends
+from fastapi import APIRouter , Depends , Query
 from typing import Annotated
 from fastapi.responses import JSONResponse
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
-from utils import validate_access_token , get_db
+from sqlalchemy import select , text
+from utils import validate_access_token , get_db , populate_query_result
 from models import Chat
 from icecream import ic
+
 
 router = APIRouter(prefix="/chats",tags=["chat"])
 
@@ -16,10 +17,40 @@ async def get_all_available_chatroom(userData : Annotated[dict,Depends(validate_
 
     return JSONResponse(chats.all())
 
-@router.get("/{id}/members")
-async def list_chat_member() :
-    pass
+@router.get("/{chatId}/members")
+async def list_chat_member(chatId : str,userData : Annotated[dict,Depends(validate_access_token)],db : AsyncSession = Depends(get_db)) :
+    query = """
+        select P.uid , P.given_name , P.family_name , preferred_username from yott_user_belong_to_chat R
+        join yott_person P on R.uid = P.uid
+        where cid = :chatId
+    """
+    members = await db.execute(text(query),{"chatId" : int(chatId)})
+    return JSONResponse(populate_query_result(members))
 
-@router.post("/{id}/members")
-async def list_chat_member() :
-    pass
+@router.get("/{chatId}/members")
+async def list_chat_member(chatId : str,userData : Annotated[dict,Depends(validate_access_token)],db : AsyncSession = Depends(get_db)) :
+    query = """
+        select P.uid , P.given_name , P.family_name , preferred_username from yott_user_belong_to_chat R
+        join yott_person P on R.uid = P.uid
+        where cid = :chatId
+    """
+    members = await db.execute(text(query),{"chatId" : int(chatId)})
+    return JSONResponse(populate_query_result(members))
+
+@router.get("/{chatId}/history")
+async def list_chat_member(
+    chatId : str ,
+    userData : Annotated[dict,Depends(validate_access_token)],
+    limit : int = 10,
+    skip : int = 0,
+    db : AsyncSession = Depends(get_db)) :
+    query = """
+        select P.uid , P.given_name , P.family_name , P.preferred_username , M.data as msg_content , M."timestamp"
+        from yott_message M
+        join yott_person P on M.s_id = P.uid
+        where cid = :chatId order by M."timestamp" DESC
+        OFFSET :skip
+        LIMIT :limit
+    """
+    messages = await db.execute(text(query),{"chatId" : int(chatId),"skip" : skip , "limit" : limit})
+    return JSONResponse(populate_query_result(messages))
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,2 @@
 from .keycloak import validate_access_token , validate_accessToken_without_raise
-from .database import get_db
+from .database import get_db , populate_query_result
